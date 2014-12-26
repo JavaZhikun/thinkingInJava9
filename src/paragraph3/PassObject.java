2a326d0c4a2ba69f3695a3c0a2de4d1565c10c90
luanzhikun <972663246@qq.com>
2014-12-26 23:02:12 +0800

    项目提交

---
+++ src/paragraph3/PassObject.java
@@ -0,0 +1,25 @@
+package paragraph3;
+
+class Letter
+{
+	char c;
+}
+
+public class PassObject 
+{
+	static void f(Letter y)
+	{
+		y.c = 'z';
+	}
+	
+	public static void main(String[] args) 
+	{
+		Letter x = new Letter();
+		x.c = 'a';
+		System.out.println("1: x.c " + x.c);
+		f(x);
+		System.out.println("2: x.c " + x.c);
+		
+	}
+
+}
